b8d575c83c7e332f06aecc61230879d0d3ea36f9
Anmol Raibhandare <raibhandare.anmol@gmail.com>
2020-07-16 18:20:14 -0700

    Login Authentication implemented.

--- TheMovieManager/Controller/LoginViewController.swift
+++ TheMovieManager/Controller/LoginViewController.swift
@@ -23,11 +23,35 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginTapped(_ sender: UIButton) {
-        performSegue(withIdentifier: "completeLogin", sender: nil)
+        TMDBClient.getRequestToken(completion: handleRequestTokenResponse(success:error:))
     }
     
     @IBAction func loginViaWebsiteTapped() {
         performSegue(withIdentifier: "completeLogin", sender: nil)
+    }
+    
+    func handleRequestTokenResponse(success: Bool, error: Error?){
+        if success{
+            print(TMDBClient.Auth.requestToken)
+            DispatchQueue.main.async {
+                    TMDBClient.login(username: self.emailTextField.text ?? "", password: self.passwordTextField.text ?? "", completion: self.handleLoginResponse(success:Error:))
+                }
+            }
+    }
+    
+    func handleLoginResponse(success: Bool, Error: Error?){
+        print(TMDBClient.Auth.requestToken)
+        if success{
+            TMDBClient.createSessionId(completion: handleSessionResponse(success:Error:))
+        }
+    }
+    
+    func handleSessionResponse(success: Bool, Error: Error?){
+        if success{
+            DispatchQueue.main.async {
+                self.performSegue(withIdentifier: "completeLogin", sender: nil)
+            }
+        }
     }
     
 }
